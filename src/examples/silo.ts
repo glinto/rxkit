849d0d367870f9b9e7a10ecaec4c108c5bb8879b
glinto <glinto@gmail.com>
2023-06-10 01:45:45 +0200

    Add Silo example

---
+++ src/examples/silo.ts
@@ -0,0 +1,22 @@
+import { Consumer, IntervalFeeder, Silo } from "..";
+
+export class Logger<T> extends Consumer<T> {
+	consume(data: T | T[]): Promise<void> {
+		console.log(new Date(), 'Logger', data);
+		return Promise.resolve();
+	}
+}
+
+console.log('Start')
+const logger = new Logger<number>();
+const fastInterval = new IntervalFeeder({ interval: 300 });
+const slowInterval = new IntervalFeeder({ interval: 1000 });
+const silo = new Silo<number>();
+
+// The silo is fed every 300ms
+fastInterval.feeds(silo);
+// The silo, when triggered, will feed the logger
+const siloStream = silo.feeds(logger);
+// The silo's trigger will be fed every 1000ms
+slowInterval.feeds(siloStream.trigger);
+
