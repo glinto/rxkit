b2b899b88249e220c1cb5794744d474d48a3a7c3
glinto <glinto@gmail.com>
2023-06-16 13:33:35 +0200

    Each individual file must pass 80% coevarge

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,21 @@
 
 module.exports = {
-	transform: { '^.+\\.ts?$': 'ts-jest' },
-	testEnvironment: 'node',
-	testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    coverageThreshold: {
-        global: {
-          branches: 80,
-          functions: 80,
-          lines: 80,
-          statements: 80,
-        },
-      },
+  transform: { '^.+\\.ts?$': 'ts-jest' },
+  testEnvironment: 'node',
+  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+    "./src/components/*.ts": {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
