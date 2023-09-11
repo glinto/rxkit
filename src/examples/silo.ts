fd3cfa4d3bf67e4960ee464d04d07f05eae0fa57
glinto <glinto@gmail.com>
2023-09-11 22:19:41 +0200

    Use Pipes in examples for cleaner / more self-descriptive code

--- src/examples/silo.ts
+++ src/examples/silo.ts
@@ -9,16 +9,9 @@ export class Logger<T> extends Consumer<T> {
 	}
 }
 
-console.log('Start')
-const logger = new Logger<number>();
-const fastInterval = new IntervalFeeder({ interval: 300 });
-const slowInterval = new IntervalFeeder({ interval: 1000 });
-const silo = new Silo<number>();
+console.log('Start');
 
-// The silo is fed every 300ms
-fastInterval.feeds(silo);
-// The silo, when triggered, will feed the logger
-const siloStream = silo.feeds(logger);
-// The silo's trigger will be fed every 1000ms
-slowInterval.triggers(siloStream);
-
+new IntervalFeeder({ interval: 300 })
+	.pipe(new Silo())
+	.out(new Logger())
+	.triggeredWith(new IntervalFeeder({ interval: 1000 }));
--- src/examples/transformer.ts
+++ src/examples/transformer.ts
@@ -8,12 +8,13 @@ const consumer: ConsumeFunction<string> = (s) => {
 }
 
 
-const t = new Transformer((i: number) => {
+const transform = new Transformer((i: number) => {
 	if (i % 3 === 0) return `${i} can be devided by 3`;
 	return `${i} cannot be devided by 3`;
 });
-t.feeds(consumer);
 
 const iter = new Array(10).fill(0).map((_value, index) => index).values();
-const f = new IteratorFeeder(iter);
-f.feeds(t);
+
+new IteratorFeeder(iter)
+	.pipe(transform)
+	.out(consumer);
