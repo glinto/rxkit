2e335c1b2c1c8ea496e3f6fed0499292ff5f40d5
glinto <glinto@gmail.com>
2023-09-25 18:31:28 +0200

    Transmitter and Transformer to have common base

--- src/components/transformer.ts
+++ src/components/transformer.ts
@@ -1,43 +1,23 @@
-import { ConsumeFunction, Feeder, ConsumerBehavior, PushStream } from "..";
+import { ConsumeFunction, ConsumerBehavior } from "..";
+import { TransmitterBase } from "./transmitter";
 
 /**
  * A Transformer consumes one data type and feeds another type. 
  */
-export class Transformer<I, O> extends Feeder<O> implements ConsumerBehavior<I> {
+export class Transformer<I, O> extends TransmitterBase<O> implements ConsumerBehavior<I> {
 
-	protected forwardFeed?: {
-		stream: PushStream;
-		c: ConsumeFunction<O>;
-	}
 
 	constructor(private transformFn: (input: I) => O) {
 		super();
 	}
 
 	consume(data: I | I[]): Promise<void> {
-		if (this.forwardFeed === undefined)
-			return Promise.reject();
-		if (!this.forwardFeed.stream.enabled)
-			return Promise.reject();
-		if (Array.isArray(data)) {
-			return this.next(data.map(value => this.transformFn(value)), this.forwardFeed.c, this.forwardFeed.stream);
-		}
-		else {
-			return this.next(this.transformFn(data), this.forwardFeed.c, this.forwardFeed.stream);
-		}
+		return this.transmit(Array.isArray(data) ? data.map(x => this.transformFn(x)) : this.transformFn(data));
 	}
 
 	get connector(): ConsumeFunction<I> {
 		return this.consume.bind(this);
 	}
 
-	protected setupFeed(c: ConsumeFunction<O>): PushStream {
-		let stream = new PushStream();
-		this.forwardFeed = {
-			c: c,
-			stream: stream
-		};
-		return stream;
-	}
 
 }
--- src/components/transmitter.ts
+++ src/components/transmitter.ts
@@ -5,33 +5,17 @@ export interface Transmission<T> {
 	c: ConsumeFunction<T>;
 }
 
-/**
- * A Transmitter is a base class which consumes data and forward feeds it to another Feedable.  
- */
-export class Transmitter<T> extends Feeder<T> implements ConsumerBehavior<T> {
 
-	private transmissions: Transmission<T>[] = [];
+export abstract class TransmitterBase<T> extends Feeder<T> {
+	protected transmissions: Transmission<T>[] = [];
 
-	constructor() {
-		super();
-	}
-
-	consume(data: T | T[]): Promise<void> {
-
+	protected transmit(data: T | T[]) {
 		if (this.transmissions.length < 1)
 			return Promise.reject('No forward feeds in transmitter');
-		return this.transmit(data);
-	}
-
-	protected transmit(data: T | T[]): Promise<void> {
 		return Promise.any(this.transmissions.map((trans) => {
 			if (!trans.stream.enabled) return Promise.reject('Disabled stream');
 			return this.next(data, trans.c, trans.stream)
 		}));
-	}
-
-	get connector(): ConsumeFunction<T> {
-		return this.consume.bind(this);
 	}
 
 	protected setupFeed(c: ConsumeFunction<T>): PushStream {
@@ -42,5 +26,25 @@ export class Transmitter<T> extends Feeder<T> implements ConsumerBehavior<T> {
 		});
 		return stream;
 	}
+}
+
+/**
+ * A Transmitter is a base class which consumes data and forward feeds it to another Feedable.  
+ */
+export class Transmitter<T> extends TransmitterBase<T> implements ConsumerBehavior<T> {
+
+	constructor() {
+		super();
+	}
+
+	consume(data: T | T[]): Promise<void> {
+		return this.transmit(data);
+	}
+
+	get connector(): ConsumeFunction<T> {
+		return this.consume.bind(this);
+	}
+
+
 
 }
